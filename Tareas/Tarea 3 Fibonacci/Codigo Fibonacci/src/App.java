be6237a6905ba0e8079e0f2de74594a513ced9ff
Ejaredac <ejaredac@hotmail.com>
2020-09-11 13:32:53 -0500

    Tarea 3 Push Fibonacci

---
+++ Tareas/Tarea 3 Fibonacci/Codigo Fibonacci/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args){
+        int _intValor1 = 0;
+        int _intValor2 = 1;
+        int _intValor3 = 0;
+        int i=0;
+        int _intNumControl = 19100140;
+        while (_intValor3 < 89) {
+            _intValor3 = _intValor1 + _intValor2;
+            i++;
+            System.out.println("Vez " + i +"\nSuma de :\n Valor 1: " + _intValor1 + "\n Valor 2: " + _intValor2 + "\n Igual a: "  + _intValor3);
+
+            _intValor1 = _intValor2;
+            _intValor2 = _intValor3;
+        }
+        System.out.println("Se termino el programa\nMuchas Gracias por Eduardo Jared Aguirre c[ardenas 19100140");
+    }
+}
